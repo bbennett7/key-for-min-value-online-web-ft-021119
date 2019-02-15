1b97dac0727f3f8904cbe166248f2a5187509168
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-15 22:53:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  values = []
+  name_hash.each do |key, value|
+    values << value
+  end
+  counter = 0 
+  loop_cap = values.length - 1
+  min_value = values[counter]
+  while counter < loop_cap
+    if values[counter] < values[counter + 1]
+      min_value = values[counter]
+    else
+      min_value = values[counter + 1]
+    counter += 1 
+    end
+  end
+  min_value
 end
