ca4ad73bfa1f8294fbba33a92035f41b9857c977
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-15 22:58:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -20,11 +20,4 @@ def key_for_min_value(name_hash)
     counter += 1 
   end
   
-  name_hash.each do |key, value|
-    if value == min_value 
-      key
-    elsif value == {}
-      nil
-    end
-  end
 end
